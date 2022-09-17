a8c883fb4553a800f59803fce79d12aa82cf36b0
Inma Gijón <igijoninf@cifpvirgendegracia.com>
2022-09-17 09:17:09 +0200

    Primeras optimizaciones

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -6,26 +6,35 @@
     /**tenemos que alcanzar 21 puntos */
 
     let deck = [];
-    const tipos      = ['C','D','H','S'];
-    const especiales = ['A','J','Q','K'];
+    const tipos      = ['C','D','H','S'],
+          especiales = ['A','J','Q','K'];
 
-    let puntosJugador = 0,
-        puntosComputadora = 0;
+    // let puntosJugador = 0,
+    //     puntosComputadora = 0;
+    let puntosJugadores = [];
 
     //Referencias del HTML
     //Si voy a usar el elemento más de una vez, lo meto en una variable para reducir el número de 
     //llamadas a querySelector
-    const btnPedir = document.querySelector('#btnPedir');
-    const btnDetener = document.querySelector('#btnDetener');
-    const btnNuevo = document.querySelector('#btnNuevo');
+    const btnPedir = document.querySelector('#btnPedir'),
+          btnDetener = document.querySelector('#btnDetener'),
+          btnNuevo = document.querySelector('#btnNuevo');
 
-    const divCartasJugador = document.querySelector('#jugador-cartas');
-    const divCartasComputadora = document.querySelector('#computadora-cartas');
+    const divCartasJugador = document.querySelector('#jugador-cartas'),
+          divCartasComputadora = document.querySelector('#computadora-cartas'),
+          smalls = document.querySelectorAll('small');
 
-    const smalls = document.querySelectorAll('small');
+
+    const inicializarJuego = ( numeroJugadores = 2 ) => {
+        deck = crearDeck();
+        for ( let i = 0; i < numeroJugadores; i++ ) {
+            puntosJugadores.push[0];
+        }
+    }
 
     //Crea una baraja
     const crearDeck = () => {
+        deck = [];
         for(let i = 2; i<=10; i++) {
             for( let tipo of tipos ) {
                 deck.push( i + tipo);
@@ -41,36 +50,28 @@
         //Vamos a utilizar una librería para barajar las cartas. Ofrece muchas funciones de 
         //JS que debería tener por defecto pero algunas no...  como el shuffle... que recibe un array 
         //y lo devuelve de forma aleatoria.
-        deck = _.shuffle(deck);
-        return deck;
+        return _.shuffle(deck);
     }
 
-
-    crearDeck();
 
     const pedirCarta = () => {
         if (deck.length === 0) {
             throw 'No hay cartas en el deck';
         }
-        const carta = deck.pop();
-        return carta;
+        return deck.pop();
     }
 
     const valorCarta = ( carta ) => {
         //Extraer el número que será el valor de la carta en el caso de las numéricas
         const valor = carta.substring(0, carta.length-1);
-
-        // let puntos = 0;
-        // if( isNaN(valor) ) {
-        //     puntos = ( valor === 'A' ) ? 11 : 10;
-        // } else {
-        //     puntos = valor*1; //Esto lo convierte en numérico
-        // }
-        // return puntos;
         
         //Esto es equivalente a lo de arriba
         return isNaN(valor) ? 
             ((valor === 'A') ? 11: 10): valor*1; 
+        
+    }
+
+    const acumularPuntos = () => {
         
     }
 
@@ -141,8 +142,8 @@
     });
 
     btnNuevo.addEventListener('click', () => {
-        deck = [];
-        deck = crearDeck();
+        console.clear();
+        inicializarJuego();
         puntosJugador = 0;
         puntosComputadora = 0;
         smalls[0].innerText =0;
